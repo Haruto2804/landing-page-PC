0d83508490aee7019cb8d16d5526eae4527ed070
= <=>
2025-11-20 10:06:12 +0700

    feat (body): add overlay when we click hamburger menu to open mobile-nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
 const mobileNavElement = document.getElementById("mobile-nav");
 const menuToggleElement = document.getElementById("menu-toggle");
+const overlayElement = document.getElementById('overlay');
 console.log(menuToggleElement)
 let isClicked = false;
 menuToggleElement.addEventListener('click', (e) => {
   isClicked = !isClicked;
   console.log(isClicked);
   mobileNavElement.classList.toggle('display');
+  overlayElement.classList.toggle('openOverlay');
+
   if (isClicked) {
     menuToggleElement.innerHTML = '<i class="animate-bounce fa-solid fa-xmark fa-lg" style="color: #ffffff;"></i>';
   } else {
